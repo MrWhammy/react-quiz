82ac99aa2baa4160a5c6d5c83e581c12b97b1da9
Brecht Yperman <brecht@yperman.eu>
2022-11-25 13:57:01 +0100

    Dat kan eleganter, maar er is geen tijd meer

--- download.mjs
+++ download.mjs
@@ -3,17 +3,46 @@ import { parse } from '@vanillaes/csv';
 import { downloadSheet, ROUNDS_SHEET } from "./src/questions.mjs";
 import fetch from 'node-fetch';
 
+function writeImage(response, roundIndex, questionIndex) {
+    const contentType = response.headers.get('content-type');
+    const extension = getExtension(contentType);
+    const imagePath = `/offline/${roundIndex}_${questionIndex}.${extension}`;
+    response.body.pipe(createWriteStream(`./public${imagePath}`));
+    return imagePath;
+}
+
+function getExtension(contentType) {
+    switch (contentType) {
+        case "image/jpeg":
+        case "image/jpg":
+            return "jpg";
+        case "image/webp":
+            return "webp";
+        case "image/gif":
+            return "gif";
+        case "image/png":
+            return "png";
+        default:
+            console.error(`Unknown content type ${contentType}`)
+    }
+}
+
 function download() {
     downloadSheet(ROUNDS_SHEET).then(rounds => {
         writeFileSync(`./public/offline/${ROUNDS_SHEET}.csv`, rounds);
 
         parse(rounds).splice(1).forEach((round, roundIndex) => {
             downloadSheet(round[0]).then(questions => {
-                parse(questions).splice(1).forEach((question, questionIndex) => {
-                    let imgUrl = question[2];
-                    fetch(imgUrl).then(response => response.body.pipe(createWriteStream(`./public/offline/${roundIndex}_${questionIndex}`)));
-                })
-                writeFileSync(`./public/offline/${round[0]}.csv`, questions);
+                Promise.all(parse(questions).splice(1).map((question, questionIndex) => {
+                    let imageUrl = question[2];
+                    return fetch(imageUrl).then(response => writeImage(response, roundIndex, questionIndex)).then(newUrl => { return { imageUrl: imageUrl, newUrl: newUrl };});
+                })).then(newUrls => {
+                    let newQuestions = questions;
+                    newUrls.forEach(mapping => {
+                       newQuestions = newQuestions.replace(mapping.imageUrl, mapping.newUrl);
+                    });
+                    writeFileSync(`./public/offline/${round[0]}.csv`, newQuestions);
+                });
             })
         });
     });
